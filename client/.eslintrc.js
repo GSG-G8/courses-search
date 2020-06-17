f318635781280988c266f5715a37b099ccbbbf41
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-06-17 13:51:01 +0300

    eslint init

---
+++ client/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    extends: ["airbnb", "prettier", "prettier/react"],
+    parser: "babel-eslint",
+    parserOptions: {
+      ecmaVersion: 8,
+      ecmaFeatures: {
+        impliedStrict: true
+      }
+    },
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+      jest: true
+    },
+    rules: {
+      "arrow-body-style": ["error", "as-needed"],
+      "react/state-in-constructor": 0,
+      "react/jsx-filename-extension": [
+        1,
+        {
+          extensions: [".js", ".jsx"]
+        }
+      ],
+      quotes: [
+        2,
+        "single",
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: true
+        }
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "es5",
+          singleQuote: true
+        }
+      ]
+    },
+    plugins: ["react", "prettier"]
+  };
+  
--- client/src/App/index.js
+++ client/src/App/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      courses search
-    </div>
-  );
+  return <div className="App">courses search</div>;
 }
 
 export default App;
