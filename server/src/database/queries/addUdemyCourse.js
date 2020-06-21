b5f7b6179dc24c0464957d10390c8aa280abefce
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-06-21 19:18:12 +0300

    make query to insert udemy course to database relates #15

---
+++ server/src/database/queries/addUdemyCourse.js
@@ -0,0 +1,31 @@
+const connection = require('../config/connection');
+
+const addUdemyCourse = (data) => {
+  const {
+    title,
+    categoryId,
+    image_480x270: image,
+    author,
+    url,
+    rating,
+    num_reviews: reviews,
+    objectives_summary: description,
+    source,
+  } = data;
+  return connection.query(
+    'INSERT INTO course (category_id, title, image, author_name, url, rate, reviews, description, source) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+    [
+      categoryId,
+      title,
+      image,
+      author,
+      url,
+      rating,
+      reviews,
+      description[0],
+      source,
+    ]
+  );
+};
+
+module.exports = addUdemyCourse;
---
+++ server/src/database/queries/index.js
@@ -0,0 +1,5 @@
+const addUdemyCourse = require('./addUdemyCourse');
+
+module.exports = {
+  addUdemyCourse,
+};
