d0c01a9b212a468ebdd874bbabf67309b15cc06f
lina-jamal <linahjamal89@gmail.com>
2020-06-21 10:45:18 +0300

    edit t getcourseByCatId  query

--- server/src/database/queries/course.js
+++ server/src/database/queries/course.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection');
 
 const getcourseByCatId = (catId) =>
   connection.query({
-    text: `SELECT title, image
+    text: `SELECT title, image, rate, source
         FROM course WHERE category_id=$1;`,
     values: [catId],
   });
