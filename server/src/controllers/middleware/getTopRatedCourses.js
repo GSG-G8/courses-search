04f19bc44fdadd41b5fafcf8f0e1917634212f11
rawan <rawan.designer@hotmail.com>
2020-06-22 23:54:58 +0300

    Edit the file structure and content

--- server/src/controllers/middleware/getTopRatedCourses.js
+++ server/src/controllers/middleware/getTopRatedCourses.js
@@ -2,7 +2,6 @@ const { getTopCourses } = require('../../database/queries');
 
 const getTopRatedCourses = async (req, res, next) => {
   try {
-    console.log(res);
     const { rows } = await getTopCourses();
     res.json(rows);
   } catch (err) {
