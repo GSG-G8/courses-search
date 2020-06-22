8bd0e8d47ff51e4c3603b15ebe8797d12ab581d2
lina-jamal <linahjamal89@gmail.com>
2020-06-22 15:54:04 +0300

    make tests for route /:categoryId/courses

--- server/__test__/course.test.js
+++ server/__test__/course.test.js
@@ -8,24 +8,42 @@ describe('get all courses', () => {
   beforeAll(() => dbBuild());
   afterAll(() => connection.end());
 
-  it('get all courses by category id "1"', () =>
-    new Promise((done) => {
-      request(app)
-        .get('/api/v1/1/courses')
-        .set('Content-Type', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end((err, res) => {
-          if (err) return done(err);
-          const { data } = res.body;
-          expect(data.id).toBe(1);
-          expect(data.title).toBe('java');
-          expect(data.image).toBe(
-            'https://uniweb.qwebbuilder.com.ng/images/onlinecourses.jpg'
-          );
-          expect(data.rate).toBe('3');
+  it('get all courses by category id "1"', async () => {
+    expect.assertions(5);
+    await request(app)
+      .get('/api/v1/1/courses')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then((response) => {
+        const { data } = response.body;
+        expect(data[0].id).toBe(1);
+        expect(data[0].title).toBe('computer');
+        expect(data[0].image).toBe(
+          'https://uniweb.qwebbuilder.com.ng/images/onlinecourses.jpg'
+        );
+        expect(data[0].rate).toBe('3.4');
+        expect(data[0].source).toBe('udemy');
+      });
+  });
 
-          return done();
-        });
-    }));
+  it("should return all courses with category-id doesn't exist", async () => {
+    expect.assertions(1);
+    const res = await request(app)
+      .get('/api/v1/20/courses')
+      .set('Accept', 'application/json')
+      .expect(404);
+    const { message } = res.body;
+    expect(message).toBe("Sorry There's no courses for this category..!");
+  });
+
+  it('route /:categoryId/courses with wrong categortId status 400', async () => {
+    expect.assertions(1);
+    const res = await request(app)
+      .get('/api/v1/lina/courses')
+      .set('Accept', 'application/json')
+      .expect(400)
+      .expect('Content-Type', /json/);
+    const { message } = res.body;
+    expect(message).toBe('wrong category Id');
+  });
 });
