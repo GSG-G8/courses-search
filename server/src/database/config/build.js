2b4dc98475be3166dc9a2d94147ceff180328ed9
lina-jamal <linahjamal89@gmail.com>
2020-06-22 16:32:17 +0300

    remove comments

--- server/src/database/config/build.js
+++ server/src/database/config/build.js
@@ -3,18 +3,7 @@ const { join } = require('path');
 
 const connection = require('./connection');
 
-// const dbBuild = () => {
-//   const dbFile = readFileSync(join(__dirname, 'build.sql')).toString();
-
-//   return connection.query(dbFile);
-// };
 const dbBuild = () => {
-  // let sql = readFileSync(join(__dirname, 'build.sql')).toString();
-  // sql += readFileSync(join(__dirname, 'category.sql')).toString();
-  // if (process.env.NODE_ENV === 'test') {
-  //   sql += readFileSync(join(__dirname, 'fakeData.sql')).toString();
-  // }
-  // return connection.query(sql);
   const dbFile = readFileSync(join(__dirname, 'build.sql')).toString();
   const category = readFileSync(join(__dirname, 'category.sql')).toString();
   const fakeData = readFileSync(join(__dirname, 'fakeData.sql')).toString();
