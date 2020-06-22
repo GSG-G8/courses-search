57e9909bb38771524a92f125f166dc18dcb9deca
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-06-22 13:22:00 +0300

    make condition for fakeData build

--- server/src/database/config/build.js
+++ server/src/database/config/build.js
@@ -7,8 +7,10 @@ const dbBuild = () => {
   const dbFile = readFileSync(join(__dirname, 'build.sql')).toString();
   const categoryFile = readFileSync(join(__dirname, 'category.sql')).toString();
   const fakeData = readFileSync(join(__dirname, 'fakeData.sql')).toString();
-
-  return connection.query(dbFile + categoryFile + fakeData);
+  return connection
+    .query(dbFile)
+    .then(() => connection.query(categoryFile))
+    .then(() => connection.query(fakeData));
 };
 
 module.exports = dbBuild;
