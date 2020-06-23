9e034906a61ba9e8ff1010956bf7bbd4ade1f4e6
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-06-23 11:38:20 +0300

    fix connection.js

--- server/src/database/config/connection.js
+++ server/src/database/config/connection.js
@@ -1,14 +1,20 @@
 require('env2')('../.env');
 const { Pool } = require('pg');
 
-const keys = {
-  development: 'DB_URL',
-  production: 'DATABASE_URL',
-  test: 'TEST_DB_URL',
-};
-
-const dbUrl = process.env[keys[process.env.NODE_ENV]];
-if (!dbUrl) throw new Error('No Database URL!!!');
+let dbUrl;
+switch (process.env.NODE_ENV) {
+  case 'development':
+    dbUrl = process.env.DB_URL;
+    break;
+  case 'production':
+    dbUrl = process.env.DATABASE_URL;
+    break;
+  case 'test':
+    dbUrl = process.env.TEST_DB_URL;
+    break;
+  default:
+    throw new Error('No Database URL!!!');
+}
 
 const options = {
   connectionString: dbUrl,
