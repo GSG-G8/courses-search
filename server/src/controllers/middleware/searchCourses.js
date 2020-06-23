b01b7706481de91e6ff484b186bcc893ac4d2aad
rawan <rawan.designer@hotmail.com>
2020-06-23 15:39:51 +0300

    Edit the query and the function for search

--- server/src/controllers/middleware/searchCourses.js
+++ server/src/controllers/middleware/searchCourses.js
@@ -7,15 +7,12 @@ const getCoursesByCatIdName = async (req, res, next) => {
   try {
     const { catId } = req.params;
     const { courseName } = req.body;
-    console.log(req.body, 5);
 
     if (catId === 0) {
       const { rows } = await getCourseByName(courseName);
-      console.log(rows, 2);
       return res.json(rows);
     }
     const { rows } = await getCourseByCatIdName(catId, courseName);
-    console.log(rows);
     return res.json(rows);
   } catch (err) {
     return next(err);
