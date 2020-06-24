830730bc6ca118db41bce6ddfcd275ab736b304f
lina-jamal <linahjamal89@gmail.com>
2020-06-24 14:16:04 +0300

    convert fetch to axios, and promise to async /await

--- client/src/container/HomePage/index.js
+++ client/src/container/HomePage/index.js
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from 'react';
-// eslint-disable-next-line no-unused-vars
 import { notification, Button, Spin } from 'antd';
+import axios from 'axios';
 
 import './style.css';
 
 const HomePage = () => {
-  // const [loading, isLoading] = useState(true);
+  const [loading, isLoading] = useState(true);
   const [topcourses, setTopCourses] = useState([]);
 
-  const fetchTopCourses = () =>
-    fetch(`/api/v1/topCourses`)
-      .then((res) => {
-        if (!res.ok) {
-          res.json().then(notification.error);
-          throw res.statusText;
-        }
-        return res.json();
-      })
-      .then((results) => {
-        // console.log(results);
-        setTopCourses(results);
-      });
-
+  const fetchTopCourses = async () => {
+    try {
+      const { data } = await axios.get(`/api/v1/topCourses`);
+      setTopCourses(data);
+      isLoading(false);
+    } catch (err) {
+      notification.error(err);
+    }
+  };
   useEffect(() => {
     fetchTopCourses();
   }, []);
   return (
     <div className="Home">
-      hi from homepage
-      {/* {topcourses} */}
+      {loading && <Spin />}
+
       {topcourses.map((course) => (
         <div key={course.id}>
           <h2>{course.title}</h2>
