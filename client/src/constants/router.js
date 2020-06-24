93c66b6dabe5abb77f86680d750517b2364403cf
rawan <rawan.designer@hotmail.com>
2020-06-24 21:29:36 +0300

    Create the router

---
+++ client/src/constants/router.js
@@ -0,0 +1,9 @@
+// import DetailsPage from '../container/DetailsPage';
+// import FavoritePage from '../container/FavoritePage';
+// import HomePage from '../container/HomePage';
+
+// const homePage = './';
+// const favoritePage = './favoritecourses';
+// const detailsPage = './coursedetails';
+
+// export default homePage, favoritePage, detailsPage;
