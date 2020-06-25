5884d7919a682f6fdae45235111d767cf9bd3693
lina-jamal <linahjamal89@gmail.com>
2020-06-25 11:36:27 +0300

    add and style rate componet

--- client/src/container/HomePage/index.js
+++ client/src/container/HomePage/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { notification, Button, Spin } from 'antd';
+import { notification, Button, Spin, Rate } from 'antd';
 import { useHistory } from 'react-router-dom';
 
 import axios from 'axios';
@@ -9,12 +9,23 @@ import './style.css';
 const HomePage = () => {
   const [loading, isLoading] = useState(true);
   const [topcourses, setTopCourses] = useState([]);
+
   const history = useHistory();
 
   const fetchTopCourses = async () => {
     try {
       const { data } = await axios.get(`/api/v1/topCourses`);
-      setTopCourses(data);
+      const courseData = data.map((course) => {
+        let rateDes = '';
+        const courseRate = parseFloat(course.rate);
+        if (courseRate >= 4.5) rateDes = 'wonderful';
+        else if (courseRate > 3.5) rateDes = 'good';
+        else if (courseRate > 2.5) rateDes = 'normal';
+        else if (courseRate > 1.5) rateDes = 'bad';
+        else rateDes = 'terrible';
+        return { ...course, rateDes };
+      });
+      setTopCourses(courseData);
       isLoading(false);
     } catch (err) {
       notification.error(err);
@@ -23,23 +34,34 @@ const HomePage = () => {
   const handleClick = (id) => {
     history.push(`/detailsPage/${id}`);
   };
+
   useEffect(() => {
     fetchTopCourses();
   }, []);
 
   return (
-    <div className="Home">
+    <div className="topRate__container">
       {loading && <Spin />}
 
       {topcourses.map((course) => (
-        <div key={course.id}>
+        <div className="topRate__course-card" key={course.id}>
           <h2>{course.title}</h2>
           <img
+            className="topRate__course-card__image"
             alt="courseImg"
             src={course.image}
-            style={{ borderTopLeftRadius: '50%' }}
+            // style={{ borderTopRightRadius: '50%' }}
           />
-          <p>Rate: {course.rate}</p>
+          {/* <p>Rate: {course.rate}</p> */}
+          <span>
+            <Rate
+              // tooltips={course.rateDes}
+              value={parseFloat(Math.round(course.rate * 2) / 2)}
+              Rate
+              allowHalf
+            />
+            <span className="ant-rate-text">{course.rateDes}</span>
+          </span>
           <h3>{course.source}</h3>
           <Button onClick={() => handleClick(course.id)} type="primary">
             {' '}
