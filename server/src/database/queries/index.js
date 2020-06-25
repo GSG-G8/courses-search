e488435e27a5ef9c957e6dd567e80c4e3208625d
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-06-25 10:53:35 +0300

    add/delete queries

--- server/src/database/queries/index.js
+++ server/src/database/queries/index.js
@@ -5,6 +5,8 @@ const addUdemyCourse = require('./addUdemyCourse');
 const getFavoriteCourse = require('./getFavoriteCourse');
 const addFavorite = require('./favorite/add');
 const deleteFavorite = require('./favorite/delete');
+const addComment = require('./comment/add');
+const deleteComment = require('./comment/delete');
 const getUserById = require('./getUserById');
 const insertCourses = require('./insertCourses');
 const addUserData = require('./addUserData');
@@ -14,6 +16,8 @@ module.exports = {
   getFavoriteCourse,
   addFavorite,
   deleteFavorite,
+  addComment,
+  deleteComment,
   getUserById,
   getTopCourses,
   getcourseByCatId,
