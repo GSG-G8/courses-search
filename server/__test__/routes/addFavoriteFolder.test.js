44c12a75761574bcdd7afdf5e8ba454e4cf698a3
MohammedAlghazali <hamood-monzer11@hotmail.com>
2020-06-26 14:05:47 +0300

    make test for /favorite/add-folder route relates #50

---
+++ server/__test__/routes/addFavoriteFolder.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const connection = require('../../src/database/config/connection');
+const dbBuild = require('../../src/database/config/build');
+
+const app = require('../../src/app');
+
+const { USER_ONE_TOKEN } = process.env;
+
+const userOneToken = `token=${USER_ONE_TOKEN}`;
+
+describe('route POST /favorite/add-folder', () => {
+  beforeAll(() => dbBuild());
+  afterAll(() => connection.end());
+
+  it('user add new favorite folder successfully', async () => {
+    expect.assertions(1);
+    const { body } = await request(app)
+      .post('/api/v1/favorite/add-folder')
+      .send({ title: 'first folder for user 1' })
+      .set('Cookie', userOneToken)
+      .expect(200);
+    expect(body).toStrictEqual({ message: 'folder add successfully' });
+  });
+
+  it('un valid title in request body', async () => {
+    expect.assertions(1);
+    const { body } = await request(app)
+      .post('/api/v1/favorite/add-folder')
+      .send({ title: undefined })
+      .set('Cookie', userOneToken)
+      .expect(400);
+    expect(body).toStrictEqual({ message: 'invalid inputs..!' });
+  });
+
+  it('if request body do not have title property', async () => {
+    expect.assertions(1);
+    const { body } = await request(app)
+      .post('/api/v1/favorite/add-folder')
+      .send({})
+      .set('Cookie', userOneToken)
+      .expect(400);
+    expect(body).toStrictEqual({ message: 'invalid inputs..!' });
+  });
+});
