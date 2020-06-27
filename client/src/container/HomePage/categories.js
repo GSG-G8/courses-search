b2bfbe3c03ec007845a79bcbf07bd262f0a50c58
lina-jamal <linahjamal89@gmail.com>
2020-06-28 01:46:30 +0300

    add catigories array

---
+++ client/src/container/HomePage/categories.js
@@ -0,0 +1,160 @@
+const categories = [
+  {
+    title: 'development',
+    value: 'c-0',
+    children: [
+      { title: 'web development', value: 1 },
+      { title: 'mobile apps', value: 2 },
+      { title: 'software engineering', value: 3 },
+      { title: 'databases', value: 4 },
+    ],
+  },
+  {
+    title: 'business',
+    value: 'c-1',
+    children: [
+      { title: 'finance', value: 5 },
+      { title: 'management', value: 6 },
+      { title: 'communications', value: 7 },
+      { title: 'data and analytics', value: 8 },
+    ],
+  },
+  {
+    title: 'lifeStyle',
+    value: 'c-2',
+    children: [
+      { title: 'food and beverage', value: 9 },
+      { title: 'arts and crafts', value: 10 },
+      { title: 'music', value: 11 },
+      { title: 'photography', value: 12 },
+    ],
+  },
+  {
+    title: 'Teaching and Academics',
+    value: 'c-3',
+    children: [
+      { title: 'science', value: 13 },
+      { title: 'math', value: 14 },
+      { title: 'online education', value: 15 },
+      { title: 'engineering', value: 16 },
+    ],
+  },
+  {
+    title: 'Marketing',
+    value: 'c-4',
+    children: [
+      { title: 'public relations', value: 17 },
+      { title: 'digital marketing', value: 18 },
+      { title: 'social media marketing', value: 19 },
+      { title: 'marketing fundamentals', value: 20 },
+    ],
+  },
+  {
+    title: 'Design',
+    value: 'c-5',
+    children: [
+      { title: 'graphic design', value: 21 },
+      { title: 'architectural design', value: 22 },
+      { title: 'design tools', value: 23 },
+      { title: 'user experience', value: 24 },
+    ],
+  },
+];
+
+// const categories = [
+//   // {
+//   //   development: [
+//   //     { 'web development': 1 },
+//   //     { 'mobile apps': 2 },
+//   //     { 'software engineering': 3 },
+//   //     { databases: 4 },
+//   //   ],
+//   // },
+//   {
+//     name: 'development',
+//     subCat: [
+//       ['web development', 1],
+//       ['mobile apps', 2],
+//       ['software engineering', 3],
+//       ['databases', 4],
+//     ],
+//   },
+//   // {
+//   //   business: [
+//   //     { finance: 5 },
+//   //     { management: 6 },
+//   //     { communications: 7 },
+//   //     { 'data and analytics': 8 },
+//   //   ],
+//   // },
+//   {
+//     name: 'business',
+//     subCat: [
+//       ['finance', 5],
+//       ['management', 6],
+//       ['communications', 7],
+//       ['data and analytics', 8],
+//     ],
+//   },
+//   // {
+//   //   lifeStyle: [
+//   //     { 'food and beverage': 9 },
+//   //     { 'arts and crafts': 10 },
+//   //     { music: 11 },
+//   //     { photography: 12 },
+//   //   ],
+//   // },
+//   {
+//     name: 'lifeStyle',
+//     subCat: [
+//       ['food and beverage', 9],
+//       ['arts and crafts', 10],
+//       ['music', 11],
+//       ['photography', 12],
+//     ],
+//   },
+//   // {
+//   //   'Teaching and Academics': [
+//   //     { science: 13 },
+//   //     { math: 14 },
+//   //     { 'online education': 15 },
+//   //     { engineering: 16 },
+//   //   ],
+//   // },
+//   {
+//     name: 'Teaching and Academics',
+//     subCat: [
+//       ['science', 13],
+//       ['math', 14],
+//       ['online education', 15],
+//       ['engineering', 16],
+//     ],
+//   },
+//   // {
+//   //   Marketing: [
+//   //     { 'public relations': 17 },
+//   //     { 'digital marketing': 18 },
+//   //     { 'social media marketing': 19 },
+//   //     { 'marketing fundamentals': 20 },
+//   //   ],
+//   // },
+//   // {
+//   //   name: 'Design',
+//   //   subCat: [
+//   //     ['graphic design', 21],
+//   //     ['architectural design', 22],
+//   //     ['design tools', 23],
+//   //     ['user experience', 24],
+//   //   ],
+//   // },
+//   // {
+//   //   Design : [
+//   //     { 'graphic design': 21 },
+//   //     { 'architectural design': 22 },
+//   //     { 'design tools': 23 },
+//   //     { 'user experience': 24 },
+//   //   ],
+//   // },
+// ];
+
+export default categories;
