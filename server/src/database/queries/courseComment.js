a6668065d2b3dd75ea0a988478334360548362b8
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-07-05 10:32:38 +0300

    add picture to user data

--- server/src/database/queries/courseComment.js
+++ server/src/database/queries/courseComment.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection');
 
 const courseComment = (id) =>
   connection.query(
-    'SELECT comment.id AS comment_id, users.name, comment.content FROM comment INNER JOIN users ON comment.user_id = users.id WHERE course_id = $1 ',
+    'SELECT comment.id AS comment_id, users.name, users.picture, comment.content FROM comment INNER JOIN users ON comment.user_id = users.id WHERE course_id = $1 ',
     [id]
   );
 
