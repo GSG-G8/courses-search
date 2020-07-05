3361281c9b9b6127eb79ad697cf017c1c0a0b0b7
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-07-05 14:57:23 +0300

    fix stay logged in bug

--- client/src/container/authContext.js
+++ client/src/container/authContext.js
@@ -1,66 +1,58 @@
 import React, { useState } from 'react';
-import { Modal, notification } from 'antd';
-import { useGoogleLogout } from 'react-google-login';
+import { notification } from 'antd';
+import { useGoogleLogin, useGoogleLogout } from 'react-google-login';
 import axios from 'axios';
-import { Login } from '../components';
 
 const AuthContext = React.createContext();
 
 function AuthProvider({ children }) {
   const [isAuth, setIsAuth] = useState(false);
   const [userInfo, setUserInfo] = useState({});
-  const [showModal, setShowModal] = useState(false);
 
-  const { signOut: googleSignOut } = useGoogleLogout({
-    clientId: process.env.REACT_APP_CLIENT_ID,
-    cookiePolicy: 'single_host_origin',
-    onLogoutSuccess: () => {
-      setIsAuth(false);
-      setShowModal(false);
-    },
-  });
-
-  const signOut = async () => {
+  const onSuccess = async ({ tokenId, profileObj }) => {
     try {
-      await axios.get('/api/v1/logout');
-      googleSignOut();
-    } catch (error) {
-      notification.error({ message: 'failed to log-out' });
+      await axios.post('/api/v1/login/google', { tokenId });
+      setIsAuth(true);
+      setUserInfo(profileObj);
+    } catch {
+      notification.error({ message: 'failed to login' });
     }
   };
 
-  const showLoginModal = () => {
-    setShowModal(true);
+  const onFailure = () => {
+    notification.error({ message: 'failed to login' });
   };
+
+  const onLogoutSuccess = async () => {
+    try {
+      await axios.get('/api/v1/logout');
+      setIsAuth(false);
+    } catch (error) {
+      notification.error({ message: 'something went wrong !' });
+    }
+  };
+
+  const { signIn, loaded } = useGoogleLogin({
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    onSuccess,
+    onFailure,
+    cookiePolicy: 'single_host_origin',
+    isSignedIn: true,
+  });
+
+  const { signOut } = useGoogleLogout({
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    cookiePolicy: 'single_host_origin',
+    onLogoutSuccess,
+  });
 
   return (
     <div>
       <AuthContext.Provider
-        value={{ isAuth, userInfo, showLoginModal, signOut }}
+        value={{ isAuth, userInfo, signOut, signIn, loaded }}
       >
         {children}
       </AuthContext.Provider>
-
-      <Modal
-        title="Login"
-        forceRender
-        visible={showModal}
-        onOk={() => {
-          setShowModal(false);
-        }}
-        onCancel={() => {
-          setShowModal(false);
-        }}
-      >
-        <p>login to save your favorite courses and add comments</p>
-        <Login
-          onSuccess={(profileObj) => {
-            setIsAuth(true);
-            setShowModal(false);
-            setUserInfo(profileObj);
-          }}
-        />
-      </Modal>
     </div>
   );
 }
