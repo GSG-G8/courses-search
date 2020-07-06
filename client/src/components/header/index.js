9e1c97c2251b157af81268a39c3dec56e20a42d3
lina-jamal <linahjamal89@gmail.com>
2020-07-06 11:11:03 +0300

    finish style

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -5,109 +5,90 @@ import { Modal, Menu, Dropdown } from 'antd';
 import { FaBookmark, FaUserAlt, AiFillHome, IoMdLogOut } from 'react-icons/all';
 import Logo from '../../assets/Logo.svg';
 import { AuthContext } from '../../container/authContext';
+import { FAVORITEPAGE, LANDING } from '../../constants/router';
 
 const Header = ({ history }) => {
-  const { userInfo, isAuth, signOut, signIn } = useContext(AuthContext);
-  // console.log(userInfo);
+  const { userInfo, isAuth, signOut, signIn, loaded } = useContext(AuthContext);
 
   const info = () => {
     Modal.info({
       title: 'Course Search',
       content: (
-        <div>
-          <p>
-            {' '}
-            The place where you will find the best courses on the internet. With
-            our app we want to help the people through searching among different
-            platform about their favorite courses, by one click you will find
-            all the top courses.
-          </p>
-        </div>
+        <p>
+          The place where you will find the best courses on the internet. With
+          our app we want to help the people through searching among different
+          platform about their favorite courses, by one click you will find all
+          the top courses.
+        </p>
       ),
-      onOk() {},
     });
   };
   const menu = (
     <Menu>
-      <Menu.Item key="1" onClick={() => history.push('/')}>
+      <Menu.Item key="1" onClick={() => history.push(LANDING)}>
         <AiFillHome />
         Home
       </Menu.Item>
 
       {isAuth && (
-        <>
-          <Menu.Item key="2" onClick={() => history.push('/FavoritePage')}>
-            <FaBookmark />
-            Favorite
-          </Menu.Item>
-          <Menu.Item key="3" onClick={signOut}>
-            <IoMdLogOut />
-            logout
-          </Menu.Item>
-        </>
+        <Menu.Item key="2" onClick={() => history.push(FAVORITEPAGE)}>
+          <FaBookmark />
+          Favorite
+        </Menu.Item>
+      )}
+      {isAuth && (
+        <Menu.Item key="3" onClick={signOut}>
+          <IoMdLogOut />
+          logout
+        </Menu.Item>
       )}
     </Menu>
   );
   return (
-    <div className="header">
-      <div className="header-container">
-        <div className="header-left">
-          <Link to="/">
-            <img className="header__logo" src={Logo} alt=" logo" />
-          </Link>
-          <div className="header__list">
-            <ul>
-              <li>
-                <NavLink
-                  className="home__list"
-                  exact
-                  to="/"
-                  activeClassName="active"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <button
-                  className="header__list__button"
-                  type="button"
-                  onClick={info}
-                >
-                  About
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="header-right">
-          {isAuth ? (
-            <>
-              <Dropdown.Button
-                className="header-right__dropdown-menu"
-                overlay={menu}
-                icon={
-                  <img
-                    alt="userImage"
-                    style={{ width: '100%' }}
-                    src={userInfo.imageUrl}
-                  />
-                }
+    <div className="header-container">
+      <div className="header-left">
+        <Link to="/">
+          <img className="header__logo" src={Logo} alt=" logo" />
+        </Link>
+        <div className="header__list">
+          <ul>
+            <li>
+              <NavLink
+                className="header__list--home"
+                exact
+                to="/"
+                activeClassName="header__list--active"
               >
-                <AuthContext>
-                  {() => `Hello, ${userInfo.givenName}!`}
-                </AuthContext>
-              </Dropdown.Button>
-            </>
-          ) : (
-            <button
-              className="header_right__button"
-              type="button"
-              onClick={signIn}
-            >
-              <FaUserAlt />
-            </button>
-          )}
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <button
+                className="header__list__button"
+                type="button"
+                onClick={info}
+              >
+                About
+              </button>
+            </li>
+          </ul>
         </div>
+      </div>
+      <div className="header-right">
+        {isAuth ? (
+          <Dropdown.Button overlay={menu} icon={<FaUserAlt />}>
+            {`Hello, ${userInfo.givenName}!`}
+          </Dropdown.Button>
+        ) : (
+          <button
+            className="header_right__button"
+            type="button"
+            onClick={signIn}
+            disabled={!loaded}
+          >
+            <FaUserAlt />
+          </button>
+        )}
       </div>
     </div>
   );
