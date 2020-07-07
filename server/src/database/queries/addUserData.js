9512f7f187646ceb00244612dbd361bbab10879a
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-07-07 16:16:41 +0300

    update user picture when login

--- server/src/database/queries/addUserData.js
+++ server/src/database/queries/addUserData.js
@@ -4,7 +4,8 @@ const addUserData = (data) => {
   const { name, email, picture } = data;
   return connection.query(
     `INSERT INTO users (name, email, picture) VALUES
-    ($1, $2, $3) ON CONFLICT (email) DO UPDATE SET name = EXCLUDED.name RETURNING *`,
+    ($1, $2, $3) ON CONFLICT (email) DO UPDATE SET name = EXCLUDED.name,
+    picture = EXCLUDED.picture RETURNING *`,
     [name, email, picture]
   );
 };
